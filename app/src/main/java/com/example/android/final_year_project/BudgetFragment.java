b1dcc9e7a1dd6057a19cfb95fd0053398459677e
TheSassyGourmet <abhanavis172@gmail.com>
2017-04-30 02:20:51 +0530

    Homescreen onBackPressed Fixed

--- app/src/main/java/com/example/android/final_year_project/BudgetFragment.java
+++ app/src/main/java/com/example/android/final_year_project/BudgetFragment.java
@@ -3,6 +3,8 @@ package com.example.android.final_year_project;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +14,23 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class BudgetFragment extends Fragment {
+    ;
 
 
     public BudgetFragment() {
         // Required empty public constructor
     }
 
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.budget);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        getActivity().setTitle(R.string.Meet);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.budget);
         return inflater.inflate(R.layout.fragment_budget, container, false);
 
     }
--- app/src/main/java/com/example/android/final_year_project/ProfileFragment.java
+++ app/src/main/java/com/example/android/final_year_project/ProfileFragment.java
@@ -3,6 +3,8 @@ package com.example.android.final_year_project;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,17 +14,22 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
-
+    //final android.support.v7.widget.Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
 
     public ProfileFragment() {
         // Required empty public constructor
     }
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.profile);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.profile);
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
