77765257ce243b34c84a330a81b0fe7bb7dcb8d4
TheSassyGourmet <abhanavis172@gmail.com>
2017-05-28 17:02:47 +0530

    Expenses Screen Unfinished with Float Add Expense Button

--- app/src/main/java/com/example/android/final_year_project/BudgetFragment.java
+++ app/src/main/java/com/example/android/final_year_project/BudgetFragment.java
@@ -1,20 +1,26 @@
 package com.example.android.final_year_project;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BudgetFragment extends Fragment {
-    ;
+    FragmentManager FM;
+    FragmentTransaction FT;
 
 
     public BudgetFragment() {
@@ -30,9 +36,28 @@ public class BudgetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.budget);
         return inflater.inflate(R.layout.fragment_budget, container, false);
 
     }
-
+    @Override
+    public void onViewCreated(View v, Bundle savedInstanceState){
+        Context context = getActivity();
+        ((AppCompatActivity) context).getSupportActionBar().setTitle(R.string.budget);
+        MaterialBetterSpinner spinner = (MaterialBetterSpinner) getActivity().findViewById(R.id.android_material_design_spinner);
+        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
+        FM= ((AppCompatActivity) context).getSupportFragmentManager();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new AddExpenseFragment();
+                int title = R.string.label_add_expense;
+                if (fragment != null) {
+                    FT= FM.beginTransaction();
+                    FT.replace(R.id.containerView, fragment);
+                    FT.addToBackStack(null);
+                    FT.commit();
+                }
+            }
+        });
+    }
 }
