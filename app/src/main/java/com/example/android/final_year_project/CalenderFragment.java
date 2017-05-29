9bb5fcec69b21cdb5d31891122cf04ced22529cc
Akanksha Bhardwaj <akankshabhardwaj377@gmail.com>
2017-05-29 06:49:47 +0530

    Task Pool

--- app/src/main/java/com/example/android/final_year_project/CalenderFragment.java
+++ app/src/main/java/com/example/android/final_year_project/CalenderFragment.java
@@ -1,15 +1,16 @@
 package com.example.android.final_year_project;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
-import static com.example.android.final_year_project.R.id.toolbar;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 
 /**
@@ -35,6 +36,14 @@ public class CalenderFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onViewCreated(View v, Bundle savedInstanceState) {
+        Context context = getActivity();
+        ((AppCompatActivity) context).getSupportActionBar().setTitle(R.string.Calender);
+        MaterialBetterSpinner spinner = (MaterialBetterSpinner) getActivity().findViewById(R.id.android_material_design_spinner);
+        CalendarView simpleCalendarView = (CalendarView) getActivity().findViewById(R.id.simpleCalendarView); // get the reference of CalendarView
+        long selectedDate = simpleCalendarView.getDate();
 
+    }
 
 }
